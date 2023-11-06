17962bf94860265b887ea638c89afb9048a66c76
Krathish K <krathishs.k123@gmail.com>
2023-11-06 09:48:22 +0530

    Connect to firebase for authentication

---
+++ watchflix/src/utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASro6sXdoSC0f49-gYAt_EdolfREt_c-o",
+  authDomain: "watchflix-8e394.firebaseapp.com",
+  projectId: "watchflix-8e394",
+  storageBucket: "watchflix-8e394.appspot.com",
+  messagingSenderId: "780062919732",
+  appId: "1:780062919732:web:586169be796bc9dadd0e61",
+};
+
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
