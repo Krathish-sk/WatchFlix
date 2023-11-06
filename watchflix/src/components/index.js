f3f1ffc433d1c3b5886d479edaa5f8f80acb3569
Krathish K <krathishs.k123@gmail.com>
2023-11-06 16:36:58 +0530

    Navbar after login/register

--- watchflix/src/components/index.js
+++ watchflix/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import BgImage from "./BgImage";
 import NotAvailable from "./NotAvailable";
+import Navbar from "./Navbar";
 
-export {Header, BgImage, NotAvailable}
+export { Header, BgImage, NotAvailable, Navbar };
