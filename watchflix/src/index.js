05035e67ce932a0f0a6b546db415e3e81ed7d7fe
Krathish K <krathishs.k123@gmail.com>
2023-11-07 10:34:06 +0530

    Store setup

--- watchflix/src/index.js
+++ watchflix/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Toaster />
-    <App />
+    <Provider store={store}>
+      <Toaster />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
