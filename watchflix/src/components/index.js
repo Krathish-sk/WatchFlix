f0d4ad9a2d05c34547e8a152107e52cae3e12925
Krathish K <krathishs.k123@gmail.com>
2023-11-07 15:48:47 +0530

    Movie card setup

--- watchflix/src/components/index.js
+++ watchflix/src/components/index.js
@@ -2,5 +2,8 @@ import Header from "./Header";
 import BgImage from "./BgImage";
 import NotAvailable from "./NotAvailable";
 import Navbar from "./Navbar";
+import Slider from "./Slider";
+import CardSlider from "./CardSlider";
+import Card from "./Card";
 
-export { Header, BgImage, NotAvailable, Navbar };
+export { Header, BgImage, NotAvailable, Navbar, Slider, CardSlider, Card };
